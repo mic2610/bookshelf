9d2219d7f9074e5d43b653e4d8a8c40a472bb58c
Michael Ayoub <michael.ayoub@levo.com.au>
2024-07-26 16:17:45 +1000

    Wxercise 3 with all extra credit

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -1,36 +1,75 @@
 /** @jsx jsx */
+import React from 'react';
 import {jsx} from '@emotion/core'
 
 import './bootstrap'
 import Tooltip from '@reach/tooltip'
-import {FaSearch} from 'react-icons/fa'
+import { FaSearch, FaTimes } from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks';
+import * as colors from './styles/colors';
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
+  // Exercises 1, 2, Exra Credit 1
+  // const [status, setStatus] = React.useState('idle');
+  // const [data, setData] = React.useState(null);
+  // const [query, setQuery] = React.useState('');
+  // const [queried, setQueried] = React.useState(false);
+  // const [error, setError] = React.useState(null);
+
+  // const {data, error, run, isLoading, isError, isSuccess} = useAsync()
+
+  // React.useEffect(() => {
+  //   if (!queried) {
+  //     return;
+  //   }
+
+  //   // 🐨 call the client with the query and then update the status and data
+  //   // based on the response.
+
+  //   client(`books?query=${encodeURIComponent(query)}`).then(
+  //     data => {
+  //       setData(data);
+  //       console.log(data);
+  //       setStatus('success');
+  //       setError(null);
+  //     },
+  //     error => {
+  //       setStatus('error');
+  //       setError(error);
+  //       console.error(error);
+  //     }
+  //   );
+
+  // }, [query, queried])
 
   // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+
+  const [query, setQuery] = React.useState('');
+  const [queried, setQueried] = React.useState(false);
+  const {data, error, run, isLoading, isError, isSuccess} = useAsync();
+  
+  React.useEffect(() => {
+    if (!queried) {
+      return;
+    }
+
+    run(client(`books?query=${encodeURIComponent(query)}`));
+    
+    }, [query, queried, run]);
 
   function handleSearchSubmit(event) {
+    event.preventDefault();
     // 🐨 call preventDefault on the event so you don't get a full page reload
     // 🐨 set the queried state to true
     // 🐨 set the query value which you can get from event.target.elements
     // 💰 console.log(event.target.elements) if you're not sure.
+    setQueried(true);
+    setQuery(event.target.elements.search.value);
+    console.log(event.target.elements);
   }
 
   return (
@@ -54,12 +93,22 @@ function DiscoverBooksScreen() {
                 background: 'transparent',
               }}
             >
-              {isLoading ? <Spinner /> : <FaSearch aria-label="search" />}
+              {isLoading
+                ? <Spinner />
+                : isError ? <FaTimes aria-label="error" css={{color: colors.danger}} /> : <FaSearch aria-label="search" />}
+
             </button>
           </label>
         </Tooltip>
       </form>
-
+      {
+        isError ? (
+          <div css={{color: colors.danger}}>
+            <p>There was an error:</p>
+            <pre>{error.message}</pre>
+          </div>
+        ) : null
+      }
       {isSuccess ? (
         data?.books?.length ? (
           <BookListUL css={{marginTop: 20}}>
--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -6,6 +6,21 @@ function client(endpoint, customConfig = {}) {
   // 🐨 call window.fetch(fullURL, config) then handle the json response
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   // 💰 here's how to get the full URL: `${process.env.REACT_APP_API_URL}/${endpoint}`
+
+  let fullURL = `${process.env.REACT_APP_API_URL}/${endpoint}`;
+  const config = {
+    method: 'GET',
+    ...customConfig,
+  };
+
+  return fetch(fullURL, config).then(async response => {
+    const data = await response.json();
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(data);
+    }
+  });
 }
 
 export {client}
